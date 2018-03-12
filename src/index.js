a5a661cb463d46bceec88ff4c7b673f66b973716
dmi3q <kundimon3@gmail.com>
2018-03-12 18:23:12 +0300

    Initial version.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let pow, trailingZeros = number;
+
+    for (let prime = 2; prime <= base; prime++) {
+        if (base % prime == 0) {
+            for (pow = 0; base % prime == 0; pow++)
+                base /= prime;
+
+            let countDiv = 0, tempCountDiv = Math.floor(number / prime);
+            while (tempCountDiv > 0) {
+                countDiv += tempCountDiv;
+                tempCountDiv = Math.floor(tempCountDiv / prime);
+            }
+
+            trailingZeros = Math.min(trailingZeros, Math.floor(countDiv / pow));
+        }
+    }
+    return trailingZeros;
 }
